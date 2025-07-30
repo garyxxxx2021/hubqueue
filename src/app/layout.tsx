5333dd4b4efb72b53985437a71b6eab4729b7440
gary yang <garyyang2021@gmail.com>
2025-07-30 05:24:59 +0000

    加一个显式的顶端加载条动画

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { AuthProvider } from '@/context/AuthContext';
+import TopLoaderWrapper from '@/components/top-loader';
 
 export const metadata: Metadata = {
   title: 'HubQueue',
@@ -25,6 +26,7 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         <AuthProvider>
+            <TopLoaderWrapper />
             {children}
             <Toaster />
         </AuthProvider>
